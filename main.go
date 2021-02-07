e24a9ef94ea43080c70f1ef2eb17d1718889a968
Ken Moini <ken@kenmoini.com>
2021-02-07 12:48:26 -0500

    collapse ReadUserIP func

--- main.go
+++ main.go
@@ -470,13 +470,8 @@ func ReadUserIP(r *http.Request) string {
 
 // ReadUserIPNoPort gets the requesting client's IP without the port so you can do a reverse DNS lookup
 func ReadUserIPNoPort(r *http.Request) string {
-	IPAddress := r.Header.Get("X-Real-Ip")
-	if IPAddress == "" {
-		IPAddress = r.Header.Get("X-Forwarded-For")
-	}
-	if IPAddress == "" {
-		IPAddress = r.RemoteAddr
-	}
+	IPAddress := ReadUserIP(r)
+
 	NoPort := strings.Split(IPAddress, ":")
 	if len(NoPort) > 0 {
 		NoPort = NoPort[:len(NoPort)-1]
